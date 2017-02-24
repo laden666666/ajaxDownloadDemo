09d248cc5c915828a9bf1cea7e238668ee0e536b
laden666666 <njz198869@gmail.com>
2017-02-25 01:32:04 +0800

    提交样式 修改部分功能

--- ajaxDownload.js
+++ ajaxDownload.js
@@ -18,7 +18,7 @@
         self._errorFn = option.error;
         self._progressFn = option.progress;
         self._loadstartFn = option.loadstart;
-
+        
         var xhr = self.xhr = new XMLHttpRequest();
         xhr.open(option.type ? option.type.toUpperCase() : 'GET', option.url, true);
         xhr.responseType = 'blob';
@@ -32,6 +32,7 @@
                 var blob = new Blob([this.response], {type: this.response.type});
                 var downloadUrl = URL.createObjectURL(blob);
                 var a = document.createElement("a");
+                a.setAttribute("target","_blank");
                 a.href = downloadUrl;
                 document.body.appendChild(a);
                 a.click();
@@ -42,24 +43,31 @@
         }
 
         xhr.onprogress = function(evt){
-            //evt.loaded;  //已经上传大小情况
-            //evt.total; 附件总大小
-            var loaded = evt.loaded;
-            var tot = evt.total;
-            typeof self._progressFn == 'function' && self._progressFn({
-                total: tot,
-                loaded: loaded,
-                percent: Math.floor(100*loaded/tot),
-            });
+        	if(this.state != 4){
+        		//evt.loaded;  //已经上传大小情况
+	            //evt.total; 附件总大小
+	            var loaded = evt.loaded;
+	            var tot = evt.total;
+	            typeof self._progressFn == 'function' && self._progressFn({
+	                total: tot,
+	                loaded: loaded,
+	                percent: Math.floor(100*loaded/tot),
+	            });
+        	}
+            
         }
     }
-
-    AjaxDownload.prototype.send = function () {
-        this.xhr.send('');
-    }
-
-    AjaxDownload.prototype.abort = function () {
-        this.xhr.abort();
+    
+    AjaxDownload.prototype = {
+    	get state (){
+	        return this.xhr.readyState;
+	    },
+	    send : function () {
+	        this.xhr.send('');
+	    },
+	    abort : function () {
+	        this.xhr.abort();
+	    }
     }
 
 })(window)
